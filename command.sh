b5e131b3302b1c36cae4dd21d32070ca56fbc0ef
lfaino <luigi.faino@gmail.com>
2016-10-24 21:09:48 +0200

    example

--- command.sh
+++ command.sh
@@ -1,1 +1,3 @@
+sudo service mysql start
+bash /home/lorean/bin/LoReAn/third_party/conf_files/changeMysqlOprionsPASA.sh
 lorean.py --no_braker --short_reads /data/readsChr.fastq  --long_reads /data/longReadsChr8.fastq  --protein_evidence /data/subset.prot.fasta  --pasa_db testA -wd /data/test -t 7 /data/chr.fasta fusarium
