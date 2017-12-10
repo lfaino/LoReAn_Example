79efe944477f60d7f1d2138c485b2c8954e4be03
lfaino <luigi.faino@gmail.com>
2017-12-10 09:59:10 +0100

    Delete command.sh

--- command.sh
+++
@@ -1,3 +0,0 @@
-sudo service mysql start
-bash /home/lorean/bin/LoReAn/third_party/conf_files/changeMysqlOprionsPASA.sh
-lorean.py --no_braker --short_reads /data/readsChr.subset.fastq  --long_reads /data/longReadsChr8.fastq  --protein_evidence /data/subset.prot.fasta  --pasa_db testA -wd /data/test -t 7 /data/chr8.fasta fusarium
